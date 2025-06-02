e33622913eeed18d7c7f0a62e07f9093158d6018
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-06-02 11:39:06 +0300

    Add project files

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Rampastring.Tools;
+
+namespace MapCleaner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parallel.ForEach(args, arg =>
+            {
+                bool findoutSection = false;
+                IniFile mapSample = new IniFile(arg);
+
+                List<string> list = new List<string> { "YAYARD", "NAYARD", "GAYARD", "ZAYARD", "TAYARD", "XAYARD" };
+
+                foreach (var sectionName in list)
+                {
+                    if (
+                        mapSample.SectionExists(sectionName) &&
+                        mapSample.KeyExists(sectionName, "TechLevel") &&
+                        ((mapSample.GetIntValue(sectionName, "TechLevel", 0) > 10) || (mapSample.GetIntValue(sectionName, "TechLevel", 0) == -1))
+                       )
+                    {
+                        mapSample.RemoveSection(sectionName);
+                        findoutSection = true;
+                    }
+                }
+
+                if (findoutSection)
+                {
+                    if (!mapSample.SectionExists("#include"))
+                    {
+                        mapSample.AddSection("#include");
+                        mapSample.SetStringValue("#include", "1", "yard_disable.ini");
+                        mapSample.MoveSectionToFirst("#include");
+
+                    }
+                }
+
+                mapSample.WriteIniFile();
+            });
+        }
+    }
+}
